b28746b5bcf365e9c8a8b6b663faf3b804c0ab91
Belbut <80793911+Belbut@users.noreply.github.com>
2025-09-25 13:36:16 +0100

    feat: add devise & omniauth support

--- Gemfile
+++ Gemfile
@@ -64,3 +64,11 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "devise", "~> 4.9"
+
+gem "omniauth-facebook", "~> 1.4"
+gem "omniauth-instagram", "~> 1.3"
+gem "omniauth-github", "~> 1.0"
+
+gem "omniauth-rails_csrf_protection", "~> 0.1.2"
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  devise :confirmable, :trackable
+
+  devise :omniauthable, omniauth_providers: %i[facebook instagram github]
+end
