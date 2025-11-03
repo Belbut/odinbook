54c1c7f0f414c21179995528ab77f675d61668c8
Belbut <80793911+Belbut@users.noreply.github.com>
2025-11-03 14:12:00 +0000

    feat: add post#show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :attachments
+  accepts_nested_attributes_for :attachments
 
   validates :body, length: { maximum: 500 }
 end
