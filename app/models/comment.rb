59f6831d69b0333d6b441c6f999264ad21e7a19e
Belbut <80793911+Belbut@users.noreply.github.com>
2025-11-26 17:03:23 +0000

    chore: create controller for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validate :body, presence: true, length: { maximum: 500 }
+  validates :body, presence: true, length: { maximum: 500 }
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   belongs_to :commentable, polymorphic: true
