15c3a1405b5df1a6aa24402306ac95e740acadde
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-03 13:14:59 +0000

    feat: add comments#update action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:comment][:post_id])
-    @comment = post.comments.new(post_params)
+    @comment = post.comments.new(comment_params)
     @comment.author = current_user
 
     if @comment.save # TODO: filter if the user doesnt have rights to comment
@@ -21,13 +21,22 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.find(params[:id])
   end
 
-  def update; end
+  def update
+    @comment = current_user.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      render @comment
+    else
+      flash.now[:alert] = @comment.errors.full_messages.to_sentence
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy; end
 
   private
 
-  def post_params
+  def comment_params
     params.expect(comment: [:body])
   end
 end
