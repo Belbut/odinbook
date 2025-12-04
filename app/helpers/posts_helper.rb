965c7519b6317b569c77cd603bc2386f6a8442ae
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-04 16:10:25 +0000

    refactor: post shared_content

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,24 @@
 module PostsHelper
-  def not_displayed_attachments(post)
-    "#{post.attachments.size - 5} More" if post.attachments.size > 5
+  def render_attachments_preview(post)
+    safe_join([render_first_attachments_preview(post), render_amount_not_previewed(post)])
   end
 
-  def preview_attachments(post)
-    post.attachments.first(5)
+  private
+
+  PREVIEW_SIZE = 5
+
+  def render_first_attachments_preview(post)
+    attachments_preview = post.attachments.first(PREVIEW_SIZE).map do |attachment|
+      if attachment.annexable.present? && attachment.annexable.file.representable?
+        active_storage_record = attachment.annexable.file.representation(resize_to_limit: [300, 300])
+        image_tag(active_storage_record)
+      end
+    end
+    # TODO: handle render of more than PREVIEW_SIZE 5 photos
+    safe_join(attachments_preview)
+  end
+
+  def render_amount_not_previewed(post)
+    content_tag(:span, "#{post.attachments.size - PREVIEW_SIZE} More") if post.attachments.size > PREVIEW_SIZE
   end
 end
