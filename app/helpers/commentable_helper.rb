00cc43c76130439ade69acf108b3097a1915a281
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-04 17:29:46 +0000

    refactor: rename methods and directories

--- app/helpers/commentable_helper.rb
+++ app/helpers/commentable_helper.rb
@@ -1,23 +1,23 @@
 module CommentableHelper
-  def render_commentable_children(commentable_node, children_depth = 0)
-    current_node = render(commentable_node, class_append: "child_depth_#{children_depth}")
-
-    children_nodes = commentable_node.replies.map do |reply|
-      render_commentable_children(reply, children_depth + 1)
-    end
-
-    current_node + children_nodes.join.html_safe
-  end
-
   def render_deleted_content(content)
     content_tag(:div, "This is a fragment of time, this #{content.class.name} was deleted")
   end
 
   def render_visible_content(content)
     safe_join([
-                render("shared/heading", content: content),
-                render("shared/content", content: content),
-                render("shared/interaction", content: content)
+                render("commentable/heading", content: content),
+                render("commentable/content", content: content),
+                render("commentable/interactions", content: content)
               ])
+  end
+
+  def render_thread_from(commentable_node, children_depth = 0)
+    current_node = render(commentable_node, class_append: "child_depth_#{children_depth}")
+
+    children_nodes = commentable_node.replies.map do |reply|
+      render_thread_from(reply, children_depth + 1)
+    end
+
+    safe_join([current_node, children_nodes])
   end
 end
