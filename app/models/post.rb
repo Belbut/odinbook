16743b45b469f0426eb67d0154490563bc94e35b
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-05 17:02:27 +0000

    refactor: alias post#comments to #replies to support unified commentable interface

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,11 @@ class Post < ApplicationRecord
 
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :attachments, dependent: :destroy
+
   has_many :comments, as: :commentable
-  
+  # Alias used only for polymorphic abstractions. use replies comments when post is the only type.
+  alias replies comments
+
   def attach_files(files_params)
     return if files_params.nil?
 
