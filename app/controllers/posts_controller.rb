801c9b1841b44f51684017d2748ef6acefb0d70d
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-10 17:56:13 +0000

    fix: type missing method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :limit_deleted_usage, only: %i[edit update destroy]
   def index
-    author = User.includes(:posts)
+    author = User.includes(:posts).find(user_params)
 
     @posts = author.posts.active.order(created_at: :desc)
   end
