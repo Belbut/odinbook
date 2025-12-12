dd4a56b24c60ed90f4a5b8090fc5f7b7e852fe3a
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-12 15:25:46 +0000

    fix: add module definition to attachment helper

--- app/helpers/attachment_helper.rb
+++ app/helpers/attachment_helper.rb
@@ -1,7 +1,9 @@
-def render_attachment_image(attachment)
-  return unless attachment.annexable.present? && attachment.annexable.file.representable?
+module AttachmentHelper
+  def render_attachment_image(attachment)
+    return unless attachment.annexable.present? && attachment.annexable.file.representable?
 
-  active_storage_record = attachment.annexable.file.representation(resize_to_limit: [300, 300])
-  image_tag(active_storage_record)
+    active_storage_record = attachment.annexable.file.representation(resize_to_limit: [300, 300])
+    image_tag(active_storage_record)
+  end
+  # TODO: get a better method name
 end
-# TODO: get a better method name
