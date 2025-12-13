a4ba15168fc2dbeff821a960744fa684ef6d3c6a
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-13 17:55:09 +0000

    feat: add validation for attachments image to be limited to png jpeg jpg

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,6 @@
 class Attachment < ApplicationRecord
   belongs_to :post
   belongs_to :annexable, polymorphic: true
+
+  validates_associated :annexable
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,17 @@
 class Image < ApplicationRecord
   has_one_attached :file
+  validate :file_must_be_image
 
   belongs_to :imageable, polymorphic: true, optional: true
   has_one :attachment, as: :annexable
+
+  private
+
+  def file_must_be_image
+    return unless file.attached?
+
+    return if file.blob.content_type.in?(%w[image/png image/jpeg image/jpg])
+
+    errors.add(:file, "must be a PNG or JPEG")
+  end
 end
