5e12ae6d1d95f8664c033aa948019383bcfc06f7
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-23 17:19:09 +0000

    feat: create association for friend requests and invitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   devise :confirmable, :trackable
-
   devise :omniauthable, omniauth_providers: %i[github]
 
   has_one :profile, required: true
@@ -13,4 +11,10 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+
+  has_many :relationships_out, foreign_key: "sender_id", class_name: "Relationship"
+  has_many :friendships_invitations, through: :relationships_out, source: :sender
+
+  has_many :relationships_in, foreign_key: "receiver_id", class_name: "Relationship"
+  has_many :friendships_requests, through: :relationships_in, source: :receiver
 end
