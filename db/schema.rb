fb63b454a2d84f534a2951113788702f92ee99e2
Belbut <80793911+Belbut@users.noreply.github.com>
2025-12-23 17:18:23 +0000

    feat: create db migration for relationships association between users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_17_175824) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_23_140113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -103,6 +103,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_17_175824) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "receiver_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_relationships_on_receiver_id"
+    t.index ["sender_id"], name: "index_relationships_on_sender_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -136,4 +145,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_17_175824) do
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "relationships", "users", column: "receiver_id"
+  add_foreign_key "relationships", "users", column: "sender_id"
 end
