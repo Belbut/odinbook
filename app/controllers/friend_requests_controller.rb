c8b2fbad8e3501cdcbd7175ef49d9e36508a2eeb
Belbut <80793911+Belbut@users.noreply.github.com>
2026-01-10 16:37:24 +0000

    refactor: organize friend request controller redirect and flash flow

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -4,12 +4,13 @@ class FriendRequestsController < ApplicationController
   def create
     new_friend_request = FriendRequest.new(sender: @initiator, receiver: @target)
 
-    if new_friend_request.save
-      flash[:notice] = "Friend Request Sended"
-    else
-      flash[:alert] = new_friend_request.errors.full_messages
-    end
-    redirect_back_or_to(user_profile_path(@target)) # TODO: change the fall back to friendships?
+    message = if new_friend_request.save
+                { notice: "Friend Request Sended" }
+              else
+                { alert: new_friend_request.errors.full_messages }
+              end
+
+    redirect_back_or_to(user_profile_path(@target), **message) # TODO: change the fall back to friendships?
   end
 
   def cancel
@@ -24,17 +25,15 @@ class FriendRequestsController < ApplicationController
     established_friendship_requests = FriendRequest.where(sender: [@initiator, @target],
                                                           receiver: [@target, @initiator])
 
-    if established_friendship_requests.nil?
-      return redirect_back_or_to(user_profile_path(@target),
-                                 notice: "There was no Friend Request to destroy")
-    end
+    destroyed_records = established_friendship_requests.destroy_all
 
-    if established_friendship_requests.destroy_all
-      flash[:notice] = "You ended the friendship"
-    else
-      flash[:alert] = established_friendship_requests.errors.full_messages
-    end
-    redirect_back_or_to(user_profile_path(@target)) # TODO: change the fall back to friendships?
+    message = if destroyed_records.empty?
+                { alert: "The were no relationship between the users to erase" }
+              else
+                { notice: "You ended the friendship" }
+              end
+
+    redirect_back_or_to(user_profile_path(@target), **message)
   end
 
   private
@@ -51,16 +50,14 @@ class FriendRequestsController < ApplicationController
   def destroy_friend_request(sender:, receiver:)
     friend_request = FriendRequest.find_by(sender: sender, receiver: receiver)
 
-    if friend_request.nil?
-      return redirect_back_or_to(user_profile_path(@target),
-                                 notice: "There was no Friend Request to destroy")
-    end
+    message = if friend_request.nil?
+                { notice: "The Friend Request that you referenced didn't exist" }
+              elsif friend_request.destroy
+                { notice: "Friend Request Destroyed" }
+              else
+                { alert: friend_request.errors.full_messages }
+              end
 
-    if friend_request.destroy
-      flash[:notice] = "Friend Request Destroyed"
-    else
-      flash[:alert] = friend_request.errors.full_messages
-    end
-    redirect_back_or_to(user_profile_path(@target)) # TODO: change the fall back to friendships?
+    redirect_back_or_to(user_profile_path(@target), **message) # TODO: change the fall back to friendships?
   end
 end
