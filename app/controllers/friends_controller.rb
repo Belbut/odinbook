84ffb77bcd4428df1a25e5666fd23c8d002e0666
Belbut <80793911+Belbut@users.noreply.github.com>
2026-01-15 16:13:19 +0000

    feat: add friends#index view

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,10 @@
 class FriendsController < ApplicationController
   def index
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
 
-    @user_friends = user.friends
+    @user_friends = @user.friends
 
-    return unless current_user == user
+    return unless current_user == @user
 
     @pending_incoming_fr_users = current_user.pending_incoming_friend_request_users
     @pending_outgoing_fr_users = current_user.pending_outgoing_friend_request_users
