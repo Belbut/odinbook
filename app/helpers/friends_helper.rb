9199b2619734641e8929029017b59ac857aea65b
Belbut <80793911+Belbut@users.noreply.github.com>
2026-01-16 14:04:08 +0000

    refactor: add render_users_relationships helper method

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,2 +1,11 @@
 module FriendsHelper
+  def render_users_relationships(user_array, section_heading:)
+    return if user_array.empty?
+
+    heading = content_tag(:h2, section_heading)
+    profiles = user_array.map do |user|
+      render(user.profile)
+    end
+    content_tag(:section, safe_join([heading, profiles]))
+  end
 end
