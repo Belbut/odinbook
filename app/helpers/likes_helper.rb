dc298a89f3efa365f70e092a8f3bb90c908e6cdc
Belbut <80793911+Belbut@users.noreply.github.com>
2026-03-05 14:59:25 +0000

    refactor: make like button polymorphic

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,9 +1,18 @@
 module LikesHelper
-  def like_toggle(content)
+  def render_like_toggle(content)
     if content.likes.include?(current_user)
-      button_to "Un-like", post_likes_path(content), method: :delete
+      button_to "Un-like", content_likes_path(content), method: :delete
     else
-      button_to "Like", post_likes_path(content), method: :post
+      button_to "Like", content_likes_path(content), method: :post
+    end
+  end
+
+  def content_likes_path(content)
+    case content
+    when Post
+      post_likes_path(content)
+    when Comment
+      comment_likes_path(content)
     end
   end
 end
