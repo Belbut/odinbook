2f0e52146621a273f214af38a2dac1df31a5a27f
Belbut <80793911+Belbut@users.noreply.github.com>
2026-03-10 17:46:03 +0000

    refactor: add nested_attributes to delegated types

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,6 @@
 class Content < ApplicationRecord
   delegated_type :contentable, types: %w[Post Comment], dependent: :destroy
+  accepts_nested_attributes_for :contentable
 
   belongs_to :author, class_name: "User", foreign_key: :user_id
   validates :body, length: { maximum: 500 }
@@ -7,4 +8,10 @@ class Content < ApplicationRecord
   default_scope { where(deleted: false) }
   scope :active, -> {}
   scope :deleted, -> { unscope(deleted: false).where(deleted: true) }
+
+  def self.new_with_post(author:, category: nil, **attrs)
+    new(contentable: Post.new(category: category), author: author, **attrs)
+
+    green_refactor = new(contentable: Post.new(category: category, author: author), author: author, **attrs)
+  end
 end
