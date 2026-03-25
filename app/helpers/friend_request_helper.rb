8e535fe55d8824aa9987a3ca9780bf1c188c7216
Belbut <80793911+Belbut@users.noreply.github.com>
2026-03-25 13:07:26 +0000

    style: add grid and profiles cells

--- app/helpers/friend_request_helper.rb
+++ app/helpers/friend_request_helper.rb
@@ -11,7 +11,8 @@ module FriendRequestHelper
                       turbo_confirm: "Are you sure you want unfriend #{target_user.profile.name}?" })
 
     when :ONLY_CURRENT_USER_SENDED_REQUEST
-      link_to("Cancel Friend Request", cancel_user_friend_request_path(target_user), data: { turbo_method: :delete })
+      link_to("Cancel Friend Request", cancel_user_friend_request_path(target_user),
+              data: { turbo_method: :delete })
 
     when :ONLY_TARGET_USER_SENDED_REQUEST
       accept = link_to("Accept Friend Request", user_friend_request_path(target_user), data: { turbo_method: :post })
