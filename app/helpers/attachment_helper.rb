ff0ac7601325507b023844cfd76a2a4d3a632d93
Belbut <80793911+Belbut@users.noreply.github.com>
2026-04-15 21:38:26 +0100

    style: photo grid

--- app/helpers/attachment_helper.rb
+++ app/helpers/attachment_helper.rb
@@ -1,8 +1,8 @@
 module AttachmentHelper
-  def render_attachment_image(attachment)
+  def render_attachment_image(attachment, size: [300,300])
     return unless attachment.annexable.present? && attachment.annexable.file.representable?
 
-    active_storage_record = attachment.annexable.file.representation(resize_to_limit: [300, 300])
+    active_storage_record = attachment.annexable.file.representation(resize_to_fill: size)
     image_tag(active_storage_record)
   end
   # TODO: get a better method name
