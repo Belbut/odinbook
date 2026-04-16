939859c89083e740b6a88ba3096a85bb8233dd91
Belbut <80793911+Belbut@users.noreply.github.com>
2026-04-16 12:34:30 +0100

    style: comment form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector(".file-input");
+
+  if (input) {
+    input.onchange = function () {
+      const name = this.closest(".file").querySelector(".file-name");
+
+      name.textContent =
+        this.files.length > 1
+          ? this.files.length + " files selected"
+          : this.files[0]?.name || "No file selected";
+    };
+  }
+});
