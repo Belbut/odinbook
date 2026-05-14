2021b96e3c6fae9ae0d9bfae1187ac7a9d253287
Belbut <80793911+Belbut@users.noreply.github.com>
2026-05-14 20:38:55 +0100

    fix: devise account creation with profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  devise :confirmable, :trackable
+         :rememberable, :validatable, :trackable
   devise :omniauthable, omniauth_providers: %i[github]
 
-  has_one :profile, required: true
+  has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
 
   has_many :posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # TODO: refactor routes with concerns. maybe write a personal blog post about it.
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   resources :users, only: [ :index ] do
     resource :profile, only: %i[show] do
